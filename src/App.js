6469d317e0ccf1a84f5a9b62ee19ed3a01ba4761
Jesus Gonzalez <jesus.gonzalez.ac@hotmail.com>
2021-09-14 21:39:42 -0500

    hooks/template sin terminar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Container, Navbar, Nav, Col, NavDropdown, Row } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-import Usuarios from "./routers/UsuariosRouters";
 import Formulario from "./routers/FormularioRouters";
 import Loading from "./components/Loading";
 
@@ -14,25 +13,41 @@ import MyButton from "./components/MyButton";
 
 function App() {
   const [nombres, setNombres] = useState([]);
+  const [stateModel, setStateModel] = useState({
+    name: "",
+    lastName: "",
+    age: 0,
+  });
 
-  // const [modalShow, setModalShow] = useState(false);
-  // const [btnActivo, setBtnActivo] = useState("Cargando");
-  // const [show, setShow] = useState(false);
-  // const handleShow = () => setShow(true);
+  function setData(event) {
+    const { value, name } = event.target;
+    setStateModel({ ...stateModel, [name]: value });
+  }
 
-  // const handleSelect = () => {
-  //   setModalShow(true);
-  // };
+  const [modalShow, setModalShow] = useState(false);
+  const [btnActivo, setBtnActivo] = useState("Cargando");
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
 
-  // const handleSelectFromButton = (props) => {
-  //   console.log(props.target.id);
-  //   setBtnActivo(props.target.id);
-  //   setModalShow(true);
-  // };
+  const handleSelect = () => {
+    setModalShow(true);
+  };
+
+  const handleSelectFromButton = (props) => {
+    console.log(props.target.id);
+    setBtnActivo(props.target.id);
+    setModalShow(true);
+  };
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {});
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setNombres(res.data);
+    });
   }, []);
+
+  useEffect(() => {
+    console.log(stateModel);
+  }, [stateModel]);
 
   return (
     <Router>
@@ -67,35 +82,65 @@ function App() {
             <LinkContainer to="/formulario">
               <Nav.Link>Formulario</Nav.Link>
             </LinkContainer>
-            {/* <LinkContainer to="/Formulario">
+            {<LinkContainer to="/Formulario">
               <Nav.Link>{btnActivo}</Nav.Link>
-            </LinkContainer> */}
+            </LinkContainer>}
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              {/* <NavDropdown.Item onClick={() => handleShow()}>
+              <NavDropdown.Item onClick={() => handleShow()}>
                 Canva
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item onClick={() => handleSelect()}>
                 Modal
-              </NavDropdown.Item> */}
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
 
-          {/* <MyButton /> */}
+          <MyVerticallyCenteredModal 
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            nombres={nombres}
+            // deleteTodo={deleteTodo}
+            // _handleClick={(a) => handleClick(a)}
+
+          />
+
+          <MyButton 
+          nombres={nombres}
+          _handleSelectFromButton = {(a) => handleSelectFromButton(a)}
+          />
+          
         </Navbar.Collapse>
       </Navbar>
 
       <Switch>
         <Route path={"/usuarios"}>
-          <Container style={{ padding: 30 }}>
-            <u>
-              <Usuarios />{" "}
-            </u>
-          </Container>
+          <Container style={{ padding: 30 }}/>
         </Route>
         <Route path={"/Formulario"}>
           <Container style={{ padding: 20 }}>
-            <input  placeholder={"Nombre"} />
+            <input
+              placeholder={"Nombre"}
+              id="id_name"
+              name="name"
+              onChange={(event) => setData(event)}
+            />
+          </Container>
+          <Container style={{ padding: 20 }}>
+            <input
+              placeholder={"Apellido"}
+              id="id_lastName"
+              name="lastName"
+              onChange={(event) => setData(event)}
+            />
+          </Container>
+          <Container style={{ padding: 20 }}>
+            <input
+              placeholder={"Edad"}
+              id="id_age"
+              name="age"
+              onChange={(event) => setData(event)}
+            />
           </Container>
         </Route>
       </Switch>
